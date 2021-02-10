350f5531bf91efd714f8484d69e4857f323f75c5
fruitdude <stevenrogge95@gmail.com>
2021-02-10 21:30:19 +0100

    different collisions for the box and player - disguises are now timed - disguises are now limited

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,21 +1,37 @@
 extends "res://Scripts/Character.gd"
 
 
-var _motion = Vector2()
-var _disguised = false
+var _motion : Vector2 = Vector2()
+var _disguised : bool = false
+var _velocity_multiplier : float = 1.0
 
+export var _disguise_slowdown : float = 0.25
+export var _disguise_duration : float = 5.0
+export var _number_of_disguised : int = 3
 export(NodePath) onready var _sprite = get_node(_sprite) as Sprite
 export(NodePath) onready var _light_occluder = get_node(_light_occluder) as LightOccluder2D
-export(NodePath) onready var _light = get_node(_light) as Light2D 
+export(NodePath) onready var _light = get_node(_light) as Light2D
+export(NodePath) onready var _collision = get_node(_collision) as CollisionShape2D
+export(NodePath) onready var _timer = get_node(_timer) as Timer
 export(Resource) var _player_sprite = _player_sprite as Texture
 export(Resource) var _box_sprite = _box_sprite as Texture
 export(Resource) var _human_occluder = _human_occluder as OccluderPolygon2D
 export(Resource) var _box_occluder = _box_occluder as OccluderPolygon2D
+export(Resource) var _human_collision = _human_collision as Shape
+export(Resource) var _box_collision = _box_collision as Shape
+
+
+func _ready():
+	_timer.wait_time = _disguise_duration
 
 
 func _process(delta):
 	update_motion(delta)
-	move_and_slide(_motion)
+	move_and_slide(_motion * _velocity_multiplier)
+	
+	if _disguised:
+		$DisguiseLabel.text = str(_timer.time_left).pad_decimals(2)
+		$DisguiseLabel.rect_rotation = -rotation_degrees
 	
 	
 func update_motion(delta):
@@ -49,7 +65,7 @@ func _input(event):
 func _toggle_disguise():
 	if _disguised:
 		_reveal()
-	else:
+	elif _number_of_disguised > 0:
 		_disguise()
 		
 		
@@ -59,7 +75,9 @@ func _reveal():
 	_disguised = false
 	collision_layer = 1
 	_light_occluder.occluder = _human_occluder
-	
+	_velocity_multiplier = 1
+	_collision.shape = _human_collision
+	$DisguiseLabel.hide()
 	
 	
 func _disguise():
@@ -68,3 +86,8 @@ func _disguise():
 	_disguised = true
 	collision_layer = 16
 	_light_occluder.occluder = _box_occluder
+	_velocity_multiplier = _disguise_slowdown 
+	_collision.shape = _box_collision
+	_timer.start()
+	$DisguiseLabel.show()
+	_number_of_disguised -= 1
